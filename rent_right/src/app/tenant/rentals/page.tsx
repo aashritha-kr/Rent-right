261a2d971dd9ab65027e1c26d7d598ba66d09e11
Aashritha Reddy Kukkala <166220048+aashritha-kr@users.noreply.github.com>
2025-03-25 22:50:40 +0530

    added service option in tenant rentals

--- rent_right/src/app/tenant/rentals/page.tsx
+++ rent_right/src/app/tenant/rentals/page.tsx
@@ -57,18 +57,20 @@ export default function YourRentalsPage() {
   //   },
   // ];
 
+  const Service = ["Plumbing", "Electrical", "Carpentry", "Pest Control"];
+  const [currentservice, setcurrentservice] = useState<string | null>(null);
   const [current_index, setcurrent_index] = useState<number | null>(null);
   const [description, setDescription] = useState("");
 
   useEffect(() => {
     async function fetchRentals() {
       try {
-        const response = await fetch('/api/rentals');
+        const response = await fetch("/api/rentals");
         const data = await response.json();
         setCurrentRentals(data.currentRentals);
         setPastRentals(data.pastRentals);
       } catch (error) {
-        console.error('Error fetching rental data:', error);
+        console.error("Error fetching rental data:", error);
       }
     }
 
@@ -86,64 +88,125 @@ export default function YourRentalsPage() {
       <h1 className="text-3xl font-bold text-blue-750 text-center p-6">
         YOUR RENTALS
       </h1>
-      <h2 className="text-2xl font-semibold text-blue-750 p-4">Current Rentals</h2>
+      <h2 className="text-2xl font-semibold text-blue-750 p-4">
+        Current Rentals
+      </h2>
       <div className="flex flex-col gap-6">
-        {currentRentals?currentRentals.map((rental, index) => (
-          <Card key={rental.address} className="p-4 shadow-md rounded-lg">
-            <CardContent>
-              <CardTitle className="text-lg">Address: {rental.address}</CardTitle>
-              <p className="text-blue-950">Type: {rental.type}</p>
-              <p className="text-blue-950">Start date of lease: {rental.startDate}</p>
-              <p className="text-blue-950">End date of lease: {rental.endDate}</p>
-              <p className="text-blue-950">Owner Name: {rental.Ownername}</p>
-              <p className="text-blue-950">Owner Phone Number: {rental.Ownernumber}</p>
-              <div className="flex flex-col items-start gap-2 mt-4">
-                <Button className="w-fit px-4 py-2 text-sm">View Property</Button>
-                <Button
-                  className="w-fit px-4 py-2 text-sm bg-green-700 hover:bg-green-700 text-white"
-                  onClick={() => setcurrent_index(index === current_index ? null : index)}
-                >
-                  Raise a maintenance ticket
-                </Button>
-              </div>
-              {current_index === index && (
-                <form onSubmit={handleSubmit} className="mt-4 p-4 border rounded-lg bg-gray-100">
-                  <label className="block text-gray-700 mb-2">
-                    Describe the issue (max: 300 characters):
-                  </label>
-                  <Input
-                    type="text"
-                    placeholder=""
-                    className="w-full p-2 border rounded-md"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                  />
-                  <Button type="submit" className="mt-2 bg-blue-800 text-white">
-                    Submit Request
+        {currentRentals ? (
+          currentRentals.map((rental, index) => (
+            <Card key={rental.address} className="p-4 shadow-md rounded-lg">
+              <CardContent>
+                <CardTitle className="text-lg">
+                  Address: {rental.address}
+                </CardTitle>
+                <p className="text-blue-950">Type: {rental.type}</p>
+                <p className="text-blue-950">
+                  Start date of lease: {rental.startDate}
+                </p>
+                <p className="text-blue-950">
+                  End date of lease: {rental.endDate}
+                </p>
+                <p className="text-blue-950">Owner Name: {rental.Ownername}</p>
+                <p className="text-blue-950">
+                  Owner Phone Number: {rental.Ownernumber}
+                </p>
+                <div className="flex flex-col items-start gap-2 mt-4">
+                  <Button className="w-fit px-4 py-2 text-sm">
+                    View Property
                   </Button>
-                </form>
-              )}
-            </CardContent>
-          </Card>
-        )):<Card>No ongoing rentals</Card>}
+                  <Button
+                    className="w-fit px-4 py-2 text-sm bg-green-700 hover:bg-green-700 text-white"
+                    onClick={() =>
+                      setcurrent_index(index === current_index ? null : index)
+                    }
+                  >
+                    Raise a maintenance ticket
+                  </Button>
+                </div>
+                {current_index === index && (
+                  <form
+                    onSubmit={handleSubmit}
+                    className="mt-4 p-4 border rounded-lg bg-gray-100"
+                  >
+                    <label className="block text-gray-700 mb-2">
+                      Describe the issue (max: 300 characters):
+                    </label>
+                    <Input
+                      type="text"
+                      placeholder=""
+                      className="w-full p-2 border rounded-md"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
+
+                    <label className="block text-gray-700 mb-2 mt-4">
+                      Select type of service:
+                    </label>
+                    <div className="flex flex-col gap-2">
+                      {Service.map((service) => (
+                        <label
+                          key={service}
+                          className="flex items-center gap-2 text-gray-700"
+                        >
+                          <input
+                            type="radio"
+                            name="service"
+                            value={service}
+                            checked={currentservice === service}
+                            onChange={(e) => setcurrentservice(e.target.value)}
+                            className="w-4 h-4"
+                          />
+                          {service}
+                        </label>
+                      ))}
+                    </div>
+
+                    <Button
+                      type="submit"
+                      className="mt-2 bg-blue-800 text-white"
+                    >
+                      Submit Request
+                    </Button>
+                  </form>
+                )}
+              </CardContent>
+            </Card>
+          ))
+        ) : (
+          <Card>No ongoing rentals</Card>
+        )}
       </div>
       <h2 className="text-2xl font-semibold text-blue-750 p-4">Past Rentals</h2>
       <div className="flex flex-col gap-6">
-        {pastRentals?pastRentals.map((rental) => (
-          <Card key={rental.address} className="p-4 shadow-md rounded-lg">
-            <CardContent>
-              <CardTitle className="text-lg">Address: {rental.address}</CardTitle>
-              <p className="text-blue-950">Type: {rental.type}</p>
-              <p className="text-blue-950">Start date of lease: {rental.startDate}</p>
-              <p className="text-blue-950">End date of lease: {rental.endDate}</p>
-              <p className="text-blue-950">Owner Name: {rental.Ownername}</p>
-              <p className="text-blue-950">Owner Phone Number: {rental.Ownernumber}</p>
-              <div className="flex flex-col items-start gap-2 mt-4">
-                <Button className="w-fit px-4 py-2 text-sm">View Property</Button>
-              </div>
-            </CardContent>
-          </Card>
-        )):<Card>No past rentals</Card>}
+        {pastRentals ? (
+          pastRentals.map((rental) => (
+            <Card key={rental.address} className="p-4 shadow-md rounded-lg">
+              <CardContent>
+                <CardTitle className="text-lg">
+                  Address: {rental.address}
+                </CardTitle>
+                <p className="text-blue-950">Type: {rental.type}</p>
+                <p className="text-blue-950">
+                  Start date of lease: {rental.startDate}
+                </p>
+                <p className="text-blue-950">
+                  End date of lease: {rental.endDate}
+                </p>
+                <p className="text-blue-950">Owner Name: {rental.Ownername}</p>
+                <p className="text-blue-950">
+                  Owner Phone Number: {rental.Ownernumber}
+                </p>
+                <div className="flex flex-col items-start gap-2 mt-4">
+                  <Button className="w-fit px-4 py-2 text-sm">
+                    View Property
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          ))
+        ) : (
+          <Card>No past rentals</Card>
+        )}
       </div>
     </div>
   );
